4a2b6c4f85abc91dc421d89db17b7984f378b3c0
Alysson Ajackson <alysson@alysson.net>
2015-03-09 21:56:44 -0300

    creating DAO for the classes: 'Aluno', Professor', and DAO class itself

---
+++ aula_20150302/src/br/ajackson/persistencia/AlunoDAO.java
@@ -0,0 +1,41 @@
+package br.ajackson.persistencia;
+
+import java.util.List;
+
+import br.ajackson.entidades.Aluno;
+
+public class AlunoDAO implements DAO<Aluno, Long> {
+
+	@Override
+	public Aluno find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insert(Aluno t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update(Aluno t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Aluno t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Aluno> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+
+}
