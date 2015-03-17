48ebbf3138300399d2fe7effac71d6ca88970c63
Alysson Ajackson <alysson@alysson.net>
2015-03-16 22:01:38 -0300

    JUnit test for method ProfessorDAO.find

--- aula_20150302/src/br/ajackson/Testes.java
+++ aula_20150302/src/br/ajackson/Testes.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import br.ajackson.entidades.Aluno;
+import br.ajackson.entidades.Professor;
 import br.ajackson.persistencia.AlunoDAO;
+import br.ajackson.persistencia.ProfessorDAO;
 
 public class Testes {
 	
@@ -13,6 +15,13 @@ public class Testes {
 		AlunoDAO dao = new AlunoDAO();
 		Aluno a = dao.find(1L);
 		Assert.assertEquals(a.getCPF(), "746.617.375");
+	}
+	
+	@Test
+	public void testeBuscarProfessor(){
+		ProfessorDAO prof = new ProfessorDAO();
+		Professor row = prof.find(1L);
+		Assert.assertEquals(row.getNome(), "Girafales");
 	}
 	
 }
